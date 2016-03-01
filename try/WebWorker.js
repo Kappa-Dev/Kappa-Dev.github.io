ca057b2ab374d2c71d3dc3673a172581b397fd81
KappaBot <kappa-dev@listes.sc.univ-paris-diderot.fr>
2016-03-01 16:47:16 +0000

    Sync website for js with Kappa-Dev/KaSim@1d07f057398a6a47c12e1ce59f9a056e4db93a79 add map_diff into the second field

--- try/WebWorker.js
+++ try/WebWorker.js
@@ -3533,7 +3533,7 @@
      a_d7_=caml_new_string_i_(str_s_),
      x_nM_=caml_new_string_i_("influence.dot"),
      x_nN_=caml_new_string_i_("contact.dot"),
-     version_string_iR_=caml_new_string_i_("f6a2be4"),
+     version_string_iR_=caml_new_string_i_("1d07f05"),
      match_iW_=caml_new_string_i_("profiling.txt"),
      reachability_map_0_nR_=[0,0,0,0,0,0,0,0,0,0,0],
      error_nY_=[0,0,0,0],
