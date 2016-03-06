4ad47d487eb26fa9f80df5c6636d2704cf5b1b98
KappaBot <kappa-dev@listes.sc.univ-paris-diderot.fr>
2016-03-06 14:37:22 +0000

    Sync website for js with Kappa-Dev/KaSim@18862271645c86a1f09ab09f26017cc8b1173bcd WebSim: port 8080 is indeed a standard for unpriviledged webapp

--- try/WebWorker.js
+++ try/WebWorker.js
@@ -3504,7 +3504,7 @@
      a_eb_=caml_new_string_i_(str_r_),
      x_nO_=caml_new_string_i_("influence.dot"),
      x_nP_=caml_new_string_i_("contact.dot"),
-     version_string_iM_=caml_new_string_i_("2e0bfad"),
+     version_string_iM_=caml_new_string_i_("1886227"),
      match_iR_=caml_new_string_i_("profiling.txt"),
      reachability_map_0_nT_=[0,0,0,0,0,0,0,0,0,0,0],
      error_n0_=[0,0,0,0],
