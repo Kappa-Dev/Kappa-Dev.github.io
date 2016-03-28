5db5a3d2caa2c7b88ac813ba543209d8759e93ed
KappaBot <kappa-dev@listes.sc.univ-paris-diderot.fr>
2016-03-28 08:24:41 +0000

    Sync website for js with Kappa-Dev/KaSim@44302ed3fde8549161b63fc9188469cefede3f79 Dealing with agent creations in local traces.

--- try/WebWorker.js
+++ try/WebWorker.js
@@ -3532,7 +3532,7 @@
      x_gV_=caml_new_string_i_(str_output_hI_),
      x_oF_=caml_new_string_i_("influence"),
      x_oG_=caml_new_string_i_("contact"),
-     version_string_jl_=caml_new_string_i_("2f4ca0d"),
+     version_string_jl_=caml_new_string_i_("44302ed"),
      match_jr_=caml_new_string_i_("profiling.txt"),
      error_bw_=[0,0,0,0],
      s_pi_=caml_new_string_i_("line 475"),
