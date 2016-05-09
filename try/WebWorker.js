97e6f973485b3d41529cb65cec1981f7d471c558
KappaBot <kappa-dev@listes.sc.univ-paris-diderot.fr>
2016-05-09 04:03:37 +0000

    Sync website for js with Kappa-Dev/KaSim@c96f538b63470d500fab5d063c2b0a3dfe346e95 Avoid warning messages at compilation.

--- try/WebWorker.js
+++ try/WebWorker.js
@@ -3498,7 +3498,7 @@
      x_gS_=caml_new_string_i_(str_output_hC_),
      x_oy_=caml_new_string_i_("influence"),
      x_oz_=caml_new_string_i_("contact"),
-     version_string_jn_=caml_new_string_i_("b8c856f"),
+     version_string_jn_=caml_new_string_i_("c96f538"),
      dirname_gT_=caml_new_string_i_(str_s_),
      f_fg_=caml_new_string_i_("profiling.html"),
      match_jt_=caml_new_string_i_("profiling.txt"),
