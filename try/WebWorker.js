6c3fa831774914f0916af79967931449d01a43a3
KappaBot <kappa-dev@listes.sc.univ-paris-diderot.fr>
2016-05-09 04:07:45 +0000

    Sync website for js with Kappa-Dev/KaSim@6b7ae337538b774c93b291ecca0e998002e6e373 typo

--- try/WebWorker.js
+++ try/WebWorker.js
@@ -3498,7 +3498,7 @@
      x_gS_=caml_new_string_i_(str_output_hC_),
      x_oy_=caml_new_string_i_("influence"),
      x_oz_=caml_new_string_i_("contact"),
-     version_string_jn_=caml_new_string_i_("c96f538"),
+     version_string_jn_=caml_new_string_i_("6b7ae33"),
      dirname_gT_=caml_new_string_i_(str_s_),
      f_fg_=caml_new_string_i_("profiling.html"),
      match_jt_=caml_new_string_i_("profiling.txt"),
