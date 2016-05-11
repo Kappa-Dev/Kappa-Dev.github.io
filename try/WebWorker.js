43f2b44e1719b6f6508ba1004e2542d83cd54322
KappaBot <kappa-dev@listes.sc.univ-paris-diderot.fr>
2016-05-11 16:49:45 +0000

    Sync website for js with Kappa-Dev/KaSim@c00d9ec51017437af60ccb309e6ea21cc54528cb Always bette compiling before computing the test suite.

--- try/WebWorker.js
+++ try/WebWorker.js
@@ -3496,7 +3496,7 @@
      x_gP_=caml_new_string_i_(str_output_hz_),
      x_os_=caml_new_string_i_("influence"),
      x_ot_=caml_new_string_i_("contact"),
-     version_string_jj_=caml_new_string_i_("ba9564a"),
+     version_string_jj_=caml_new_string_i_("c00d9ec"),
      dirname_gQ_=caml_new_string_i_(str_s_),
      f_fd_=caml_new_string_i_("profiling.html"),
      match_jp_=caml_new_string_i_("profiling.txt"),
