dc88056e13cef8150f309018b812b17d2c08e3ea
KappaBot <kappa-dev@listes.sc.univ-paris-diderot.fr>
2016-05-23 15:34:17 +0000

    Sync website for js with Kappa-Dev/KaSim@64a5e9500e56d8367c717c6e80b198fd361d1ce0 Sphinx is happy

--- try/WebWorker.js
+++ try/WebWorker.js
@@ -3498,7 +3498,7 @@
      x_gR_=caml_new_string_i_(str_output_hE_),
      x_od_=caml_new_string_i_("influence"),
      x_oe_=caml_new_string_i_("contact"),
-     version_string_jc_=caml_new_string_i_("f9f04c4"),
+     version_string_jc_=caml_new_string_i_("64a5e95"),
      dirname_gU_=caml_new_string_i_(str_s_),
      f_ff_=caml_new_string_i_(str_profiling_html_rg_),
      match_jn_=caml_new_string_i_(str_profiling_html_rg_),
