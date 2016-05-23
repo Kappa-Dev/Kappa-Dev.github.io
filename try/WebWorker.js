53f264c4a21fdf354981fca76a3863fe68c66b0e
KappaBot <kappa-dev@listes.sc.univ-paris-diderot.fr>
2016-05-23 23:07:23 +0000

    Sync website for js with Kappa-Dev/KaSim@4cece90e8ea99b26e277d72409662572cc5b0305 Update tutorial.rst

--- try/WebWorker.js
+++ try/WebWorker.js
@@ -3498,7 +3498,7 @@
      x_gR_=caml_new_string_i_(str_output_hE_),
      x_od_=caml_new_string_i_("influence"),
      x_oe_=caml_new_string_i_("contact"),
-     version_string_jc_=caml_new_string_i_("cf327c1"),
+     version_string_jc_=caml_new_string_i_("4cece90"),
      dirname_gU_=caml_new_string_i_(str_s_),
      f_ff_=caml_new_string_i_(str_profiling_html_rg_),
      match_jn_=caml_new_string_i_(str_profiling_html_rg_),
