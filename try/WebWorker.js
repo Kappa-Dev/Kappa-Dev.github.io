275518a2f630f228cb9ef03f21d6f32a77238f8b
KappaBot <kappa-dev@listes.sc.univ-paris-diderot.fr>
2016-06-01 14:22:50 +0000

    Sync website for js with Kappa-Dev/KaSim@828dc02c293d57db50933b645fafb1f07ffcc23e add parallel analysis, run test-case

--- try/WebWorker.js
+++ try/WebWorker.js
@@ -3499,7 +3499,7 @@
      x_gS_=caml_new_string_i_(str_output_hG_),
      x_oh_=caml_new_string_i_("influence"),
      x_oi_=caml_new_string_i_("contact"),
-     version_string_jg_=caml_new_string_i_("41fea5e"),
+     version_string_jg_=caml_new_string_i_("828dc02"),
      dirname_gV_=caml_new_string_i_(str_s_),
      f_fh_=caml_new_string_i_(str_profiling_html_rl_),
      match_jr_=caml_new_string_i_(str_profiling_html_rl_),
