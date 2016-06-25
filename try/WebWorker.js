fc5eaad8dbdc11acd0b32deca263c71607ee917a
KappaBot <kappa-dev@listes.sc.univ-paris-diderot.fr>
2016-06-25 17:24:59 +0000

    Sync website for js with Kappa-Dev/KaSim@e00bc0931ae9c4c963ca6235665dec7efb850e97 Revert "renaming export_to_KaSim into export"

--- try/WebWorker.js
+++ try/WebWorker.js
@@ -2671,7 +2671,7 @@
             [11,caml_new_string("and"),[17,[0,caml_new_string("@ "),1,0],0]]]]]]]],
      partial$4=[0,caml_new_string("@ "),1,0],
      empty_compil=[0,0,0,0,0,0,0,0,0,0],
-     version_string=caml_new_string("db735e4"),
+     version_string=caml_new_string("e00bc09"),
      a=caml_new_string("output"),
      x=caml_new_string("output"),
      x$0=caml_new_string("output"),
