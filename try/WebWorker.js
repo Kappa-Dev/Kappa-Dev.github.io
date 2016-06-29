3737abe6f6b554baa1a9ce5aaeebdfeb73eb73d4
KappaBot <kappa-dev@listes.sc.univ-paris-diderot.fr>
2016-06-29 15:05:47 +0000

    Sync website for js with Kappa-Dev/KaSim@a66bd6f923310b321966d7f6a988218c7f120593 add dynamic information into a single file

--- try/WebWorker.js
+++ try/WebWorker.js
@@ -2671,7 +2671,7 @@
             [11,caml_new_string("and"),[17,[0,caml_new_string("@ "),1,0],0]]]]]]]],
      partial$4=[0,caml_new_string("@ "),1,0],
      empty_compil=[0,0,0,0,0,0,0,0,0,0],
-     version_string=caml_new_string("0a3c29a"),
+     version_string=caml_new_string("a66bd6f"),
      a=caml_new_string("output"),
      x=caml_new_string("output"),
      x$0=caml_new_string("output"),
